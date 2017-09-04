eeb83f45059f7966878603c5bd20e86c3404137d
sarin <iamsarin94@gmail.com>
2017-09-04 14:48:16 +0700

    fix file name

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'prompypay-browser.min.js'
+    filename: 'promptypay-browser.min.js'
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
